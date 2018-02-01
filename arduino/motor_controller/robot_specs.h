56564927f3c5a4f4adae652bf0731fe8de27b800
Vibashan <vibashanvs@gmail.com>
2018-02-01 10:19:46 +0530

    Auto_Navi_Expo-ROS-

---
+++ arduino/motor_controller/robot_specs.h
@@ -0,0 +1,12 @@
+#ifndef ROBOT_SPECS_H
+#define ROBOT_SPECS_H
+
+#define encoder_pulse   13
+#define gear_ratio      120
+#define wheel_diameter  0.069   //m
+#define wheel_width     0.027   //m
+#define track_width     0.276   //m
+#define MAX_RPM         58
+#define pi              3.1415926
+#define two_pi          6.2831853
+#endif
